483a8c206652af75462e29a4a42f1a4de67d3c69
MinHtet007007 <aungminhtet770@gmail.com>
2023-09-26 14:59:33 +0630

    minor refactor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,24 +10,28 @@ const About = () => {
       <div className=" container">
         <div className="row">
           <div className=" col-12 col-lg-5 d-none d-lg-flex">
-            <img src={aboutImage} alt="computer picture" className=" w-100 rounded" />
+            <img
+              src={aboutImage}
+              alt="computer picture"
+              className=" w-100 rounded"
+            />
           </div>
           <div className=" col-12 col-lg-6 offset-lg-1">
             <div className="">
               <p className=" text-primary">About Me</p>
               <p className=" fs-3 fw-bold">
-                A dedicated Front-End Developer based in Mandalay,Myanmar.
+                A dedicated Web Developer based in Mandalay,Myanmar.
               </p>
               <p className=" text-black-50">
-                As a junior Front-End Developer, I possess an impressive arsenal
-                of skills in HTML, CSS, JavaScript, Bootstrap, React, Tailwind and
-                Sass. I excel in designing and maintaining responsive websites
-                that offer a smooth user experience. My expertise lies in
-                crafting dynamic, engaging interfaces through writing clean and
-                optimized code and utilizing cutting-edge development tools and
-                techniques. I am also a team player who thrives in collaborating
-                with cross-functional teams to produce outstanding web
-                applications.
+                As a Web Developer, I possess an impressive arsenal of skills in
+                HTML, CSS, JavaScript, Bootstrap, Sass, React, Tailwind, MySQL,
+                PHP and Laravel. I excel in designing and maintaining responsive
+                websites that offer a smooth user experience. My expertise lies
+                in crafting dynamic, engaging interfaces through writing clean
+                and optimized code and utilizing cutting-edge development tools
+                and techniques. I am also a team player who thrives in
+                collaborating with cross-functional teams to produce outstanding
+                web applications.
               </p>
             </div>
           </div>
